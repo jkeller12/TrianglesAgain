bef076ff695e544d603a4c7e6dff3f4fcd579205
Jonah Keller <jkeller10@stuy.edu>
2020-10-29 13:47:01 -0400

    The point class is now implemented

---
+++ Point.java
@@ -0,0 +1,34 @@
+public class Point{
+  private double x, y;
+
+
+
+  //construct a point given coordinates
+
+
+
+  public Point(double X, double Y)
+  {
+    x=X;
+    y=Y;
+  }
+
+  public Point(Point p)
+  {
+    x = p.x;
+    y= p.y;
+  }
+
+  public double getX()
+  {
+    return x;
+  }
+
+  public double getY()
+  {
+    return y;
+  }
+
+
+
+}
